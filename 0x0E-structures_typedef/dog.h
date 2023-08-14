ebae7b54c90492322e37f065429477e310ca392e
onuorahcharles6 <onuorahcharles6@gmail.com>
2023-08-14 05:37:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+*struct dog - A structure tag.
+*@name: The name of the dog.
+*@age: The age of the dog.
+*@owner: The owner of the dog.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
