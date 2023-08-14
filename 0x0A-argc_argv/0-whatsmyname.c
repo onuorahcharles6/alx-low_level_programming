00f1b72c19b80b7c22c5ed8c16104dd41c2e9f11
onuorahcharles6 <onuorahcharles6@gmail.com>
2023-08-14 08:30:54 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *Paul - Entry point
+ *@argc: Arguement counter.
+ *@argv: Arguement vector.
+ *Return: Returns an interger.
+ */
+
+int Paul(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+void _putchar(char c);
+int get_change(int cents);
+#endif
