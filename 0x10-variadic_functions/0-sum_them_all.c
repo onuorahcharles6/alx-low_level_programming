a2fa88046db1549b439dd2d8f02aee2b94a64be3
onuorahcharles6 <onuorahcharles6@gmail.com>
2023-08-17 13:40:02 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,27 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
 /**
- *sum_them_all - a function to return the sum of the whole parameters
- *@n: the parameter
- *Return: the integer
+ * sum_them_all - Program returns sum of its parameters.
+ * @n: identifies all the argu
+ * Return: Always 0.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
+	/* generating va_list storage 4 variable argu list */
+	va_list my_nums;
+	unsigned int count;
 	int sum;
-	unsigned int x;
-	va_list addNumbers;
-
-	va_start(addNumbers, n);
 
 	if (n == 0)
 		return (0);
-	for (x = 0; x < n; x++)
-		sum = sum + va_arg(addNumbers, int);
-	va_end(addNumbers);
+	sum = 0;
+
+	/* init va_list 4 the number of argu */
+	va_start(my_nums, n);
+
+	/* directs all argu stored in the valist */
+	for (count = 0; count < n; count++)
+		sum += va_arg(my_nums, int);
+	va_end(my_nums);
 	return (sum);
 }
