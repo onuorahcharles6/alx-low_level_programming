0ef77699194fc5734509611f47a0049d8509c112
onuorahcharles6 <onuorahcharles6@gmail.com>
2023-08-17 13:59:01 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,27 +1,28 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 /**
- *print_numbers - The function prints numbers, and a new line
- *@separator: the string printed between numbers
- *@n: the number of integers passed to the whole function
- *Return: 0
+ * print_numbers - funct prints digits.
+ * @n: shows numbers around argu
+ * @separator: identifies seperator among digits
+ * Return: As usual 0.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int x;
-	va_list print;
+	/* generating va_list then stores variable argu list */
+	va_list my_nums;
+	unsigned int count;
 
-	va_start(print, n);
+	/* init valist for the number of argu in the list */
+	va_start(my_nums, n);
 
-	if (separator == NULL)
-		separator = "";
-	for (x = 0; x < n; x++)
+	/* connects all argu stored in the va_list */
+	for (count = 0; count < n; count++)
 	{
-		printf("%d", va_arg(print, int));
-		if (x < n - 1)
+		printf("%d", va_arg(my_nums, unsigned int));
+		if (count < (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
+	va_end(my_nums);
 	printf("\n");
-	va_end(print);
 }
