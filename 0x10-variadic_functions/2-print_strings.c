5985af21e74f28d173e1d683d05e41a656fed486
onuorahcharles6 <onuorahcharles6@gmail.com>
2023-08-17 14:20:24 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,30 +1,37 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- *print_strings - The funct prints strings, and new line
- *@separator: string to be printed between strings
- *@n: number of strings passed to the funct
- *Return: nothing
+ * print_strings - Prints strings, then a new line.
+ * @separator: String printed between strings.
+ * @n: Total sum of strings given 2 d program.
+ * @...: A variabl digits of strings printed.
+ * Description: If divider is NULL, don't print.
+ * If any strings is NULL, (0) is printed instead.
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	char *put;
-	unsigned int x;
-	va_list prints;
+	va_list strings;
+	char *str;
+	unsigned int index;
 
-	va_start(prints, n);
-	if (separator == NULL)
-		separator = "";
-	for (x = 0; x < n; x++)
+	va_start(strings, n);
+
+	for (index = 0; index < n; index++)
 	{
-		put = va_arg(prints, char*);
-		if (put == NULL)
-			put = "(nil)";
-		printf("%s", put);
-		if (x < n - 1)
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
+
 	printf("\n");
-	va_end(prints);
+
+	va_end(strings);
 }
